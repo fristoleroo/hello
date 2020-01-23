8d1765aaf2c2ad71ddca8bfe8d3e4cdcc0e64aca
fristoleroo <rajamirulrasyid@gmail.com>
2020-01-23 00:32:07 -0600

    Adding a line for major

---
+++ hello.c
@@ -0,0 +1,25 @@
+
+/*
+  Programmer's Name: Raja Amirul Rasyid Raja Shahreen
+  NUID: 97584742
+  Date: 1/22/2020
+  Description of the program: Using latitude and longitude to calculate air distance between two locations
+
+  
+*/
+
+
+#include <stdio.h>
+
+
+int main (void) {  
+printf("Raja Amirul Rasyid\n");
+printf("Mathematics\n");	// Variable declarations  
+
+	// Program statements  
+
+	
+	
+	return 0;
+
+}
